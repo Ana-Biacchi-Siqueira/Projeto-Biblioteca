98d8e9bc4a62b1023cefc4212d13c0f92742a1d9
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-06-28 19:55:38 -0300

    Segundo commit

---
+++ BibliotecaProjeto/src/main/java/br/com/biblioteca/controllers/dto/AutorOutput.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.controllers.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+	@Getter
+	@Setter
+		public class AutorOutput {
+			private Long id;
+			private String nome;
+			private String biografia;
+
+}
